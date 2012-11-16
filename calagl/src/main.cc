fbdd98aeb18435a5186e19688369688f14e25c87
John Tsiombikas <nuclear@mutantstargoat.com>
2012-11-17 01:57:11 +0200

    added a gamma correction shader to the calagl frontend

--- calagl/src/main.cc
+++ calagl/src/main.cc
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <GL/glew.h>
+
 #ifndef __APPLE__
 #include <GL/glut.h>
 #else
@@ -8,14 +10,32 @@
 #endif
 
 #include "calacirya.h"
+#include "sdr.h"
 
 static bool init();
 static void cleanup();
 static void disp();
 static void reshape(int x, int y);
 static void keydown(unsigned char key, int x, int y);
+static void keyup(unsigned char key, int x, int y);
+static void passive(int x, int y);
 
 static RenderContext ctx;
+
+static Shader *postsdr;
+static SdrProg *postprog;
+
+static const char *postsdr_source =
+	"uniform sampler2D tex;\n"
+	"uniform float inv_gamma;\n"
+	"void main()\n"
+	"{\n"
+	"    vec3 col = texture2D(tex, gl_TexCoord[0].st).xyz;\n"
+	"    gl_FragColor = vec4(pow(col, vec3(inv_gamma)), 1.0);\n"
+	"}\n";
+
+static float gamma_value = 2.2;
+
 
 int main(int argc, char **argv)
 {
@@ -27,6 +47,9 @@ int main(int argc, char **argv)
 	glutDisplayFunc(disp);
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keydown);
+	glutKeyboardUpFunc(keyup);
+
+	glewInit();
 
 	if(!init()) {
 		return 1;
@@ -41,10 +64,11 @@ static bool init()
 	calacirya_init();
 
 	ctx.scn = new Scene;
-	ctx.scn->set_background(Vector3(0.2, 0.3, 0.4));
+	ctx.scn->set_background(Vector3(0.04, 0.06, 0.1));
 
 	ctx.opt.width = 640;
 	ctx.opt.height = 300;
+	ctx.opt.samples = 4;
 
 	ctx.framebuf = new Pixmap;
 	ctx.framebuf->width = ctx.opt.width;
@@ -55,8 +79,8 @@ static bool init()
 	Material *mtl = new Material;
 	mtl->add_brdf(get_brdf("lambert"));
 	mtl->add_brdf(get_brdf("phong"));
-	mtl->set_attrib("diffuse", Vector3(0.3, 0.4, 0.9));
-	mtl->set_attrib("specular", Vector3(0.8, 0.8, 0.8));
+	mtl->set_attrib("diffuse", Vector3(0.2, 0.3, 0.7));
+	mtl->set_attrib("specular", Vector3(0.6, 0.6, 0.6));
 	mtl->set_attrib("shininess", 80.0);
 
 	Sphere *sph = new Sphere;
@@ -82,6 +106,18 @@ static bool init()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, ctx.opt.width, ctx.opt.height, 0, GL_RGB, GL_FLOAT, ctx.framebuf->pixels);
 
+	// load the gamma correction shader
+	postsdr = new Shader(GL_FRAGMENT_SHADER);
+	postsdr->set_source(postsdr_source);
+
+	postprog = new SdrProg;
+	postprog->add_shader(postsdr);
+
+	if(!postprog->link()) {
+		return false;
+	}
+	bind_program(postprog);
+
 	atexit(cleanup);
 	return true;
 }
@@ -95,16 +131,33 @@ static void disp()
 {
 	static bool done_rend;
 
-	float *fbptr = ctx.framebuf->pixels;
+	postprog->set_uniform("inv_gamma", 1.0f / gamma_value);
 
-	for(int i=0; i<ctx.opt.height; i++) {
-		if(!done_rend) {
+	if(!done_rend) {
+		float *fbptr = ctx.framebuf->pixels;
+
+		for(int i=0; i<ctx.opt.height; i++) {
 			render_scanline(&ctx, i);
+
+			glTexSubImage2D(GL_TEXTURE_2D, 0, 0, i, ctx.opt.width, 1, GL_RGB, GL_FLOAT, fbptr);
+			fbptr += ctx.opt.width * 3;
+
+			glBegin(GL_QUADS);
+			glTexCoord2f(0, 1);
+			glVertex2f(-1, -1);
+			glTexCoord2f(1, 1);
+			glVertex2f(1, -1);
+			glTexCoord2f(1, 0);
+			glVertex2f(1, 1);
+			glTexCoord2f(0, 0);
+			glVertex2f(-1, 1);
+			glEnd();
+
+			glFlush();
 		}
 
-		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, i, ctx.opt.width, 1, GL_RGB, GL_FLOAT, fbptr);
-		fbptr += ctx.opt.width * 3;
-
+		done_rend = true;
+	} else {
 		glBegin(GL_QUADS);
 		glTexCoord2f(0, 1);
 		glVertex2f(-1, -1);
@@ -118,8 +171,6 @@ static void disp()
 
 		glFlush();
 	}
-
-	done_rend = true;
 }
 
 static void reshape(int x, int y)
@@ -127,9 +178,44 @@ static void reshape(int x, int y)
 	glViewport(0, 0, x, y);
 }
 
+static bool keystate[128];
+static int prev_x, prev_y;
+
 static void keydown(unsigned char key, int x, int y)
 {
 	if(key == 27) {
 		exit(0);
+	}
+
+	if(key < sizeof keystate / sizeof *keystate) {
+		keystate[key] = true;
+	}
+	glutPassiveMotionFunc(passive);
+	prev_x = x;
+	prev_y = y;
+}
+
+static void keyup(unsigned char key, int x, int y)
+{
+	if(key < sizeof keystate / sizeof *keystate) {
+		keystate[key] = false;
+	}
+	glutPassiveMotionFunc(0);
+}
+
+static void passive(int x, int y)
+{
+	int dx = x - prev_x;
+	//int dy = y - prev_y;
+
+	prev_x = x;
+
+	if(keystate['g'] || keystate['G']) {
+		gamma_value += 4.0 * (float)dx / (float)ctx.opt.width;
+		if(gamma_value < 0.01) {
+			gamma_value = 0.01;
+		}
+		printf("gamma: %f\n", gamma_value);
+		glutPostRedisplay();
 	}
 }
---
+++ calagl/src/sdr.cc
@@ -0,0 +1,285 @@
+#include <stdio.h>
+#include <map>
+#ifdef _MSC_VER
+#include <memory.h>
+#else
+#include <alloca.h>
+#endif
+#include <GL/glew.h>
+#include "sdr.h"
+
+Shader::Shader(unsigned int type)
+{
+	sdr = glCreateShader(type);
+	compiled = false;
+}
+
+Shader::~Shader()
+{
+	glDeleteShader(sdr);
+}
+
+void Shader::set_source(const char *src)
+{
+	glShaderSource(sdr, 1, &src, 0);
+	compiled = false;
+}
+
+bool Shader::compile()
+{
+	if(compiled) {
+		return true;
+	}
+
+	glCompileShader(sdr);
+
+	int len;
+	glGetShaderiv(sdr, GL_INFO_LOG_LENGTH, &len);
+	if(len) {
+		char *buf = (char*)alloca(len + 1);
+		glGetShaderInfoLog(sdr, len, &len, buf);
+		fprintf(stderr, "compiler: %s\n", buf);
+	}
+
+	int status;
+	glGetShaderiv(sdr, GL_COMPILE_STATUS, &status);
+	if(!status) {
+		fprintf(stderr, "shader compilation failed\n");
+		return false;
+	}
+
+	compiled = true;
+	return true;
+}
+
+bool Shader::is_compiled() const
+{
+	return compiled;
+}
+
+bool Shader::load(const char *fname)
+{
+	FILE *fp;
+
+	if(!(fp = fopen(fname, "rb"))) {
+		fprintf(stderr, "failed to open shader file: %s\n", fname);
+		return false;
+	}
+
+	fseek(fp, 0, SEEK_END);
+	long sz = ftell(fp);
+	rewind(fp);
+
+	char *buf = new char[sz + 1];
+	if((long)fread(buf, 1, sz, fp) < sz) {
+		fprintf(stderr, "failed to read shader contents\n");
+		fclose(fp);
+		return false;
+	}
+	fclose(fp);
+
+	set_source(buf);
+	delete [] buf;
+
+	return compile();
+}
+
+// ---- shader manager ----
+static std::map<std::string, Shader*> sdrcache;
+
+Shader *get_shader(const char *fname, unsigned int type)
+{
+	auto it = sdrcache.find(std::string(fname));
+	if(it != sdrcache.end()) {
+		return it->second;
+	}
+
+	Shader *sdr = new Shader(type);
+	if(!sdr->load(fname)) {
+		delete sdr;
+		return 0;
+	}
+	sdrcache[fname] = sdr;
+	return sdr;
+}
+
+
+// ---- class SdrProg ----
+
+SdrProg::SdrProg()
+{
+	prog = glCreateProgram();
+	linked = false;
+}
+
+SdrProg::~SdrProg()
+{
+	glDeleteProgram(prog);
+}
+
+void SdrProg::add_shader(Shader *sdr)
+{
+	if(sdr->compile()) {
+		glAttachShader(prog, sdr->sdr);
+		shaders.push_back(sdr);
+		linked = false;
+	}
+}
+
+bool SdrProg::link()
+{
+	if(linked) {
+		return true;
+	}
+
+	glLinkProgram(prog);
+
+	int len;
+	glGetShaderiv(prog, GL_INFO_LOG_LENGTH, &len);
+	if(len) {
+		char *buf = (char*)alloca(len + 1);
+		glGetProgramInfoLog(prog, len, &len, buf);
+		buf[len] = 0;
+		fprintf(stderr, "linker: %s\n", buf);
+	}
+
+	int status;
+	glGetProgramiv(prog, GL_LINK_STATUS, &status);
+	if(!status) {
+		fprintf(stderr, "program linking failed\n");
+		return false;
+	}
+
+	linked = true;
+	return true;
+}
+
+bool SdrProg::load(const char *vsfname, const char *psfname)
+{
+	Shader *sdr;
+
+	if(vsfname) {
+		if(!(sdr = get_shader(vsfname, GL_VERTEX_SHADER))) {
+			return false;
+		}
+		add_shader(sdr);
+	}
+
+	if(psfname) {
+		if(!(sdr = get_shader(psfname, GL_FRAGMENT_SHADER))) {
+			return false;
+		}
+		add_shader(sdr);
+	}
+	return link();
+}
+
+int SdrProg::get_uniform_location(const char *name)
+{
+	bind_program(this);
+	return glGetUniformLocation(prog, name);
+}
+
+int SdrProg::get_attribute_location(const char *name)
+{
+	bind_program(this);
+	return glGetAttribLocation(prog, name);
+}
+
+void SdrProg::set_uniform(const char *name, int val)
+{
+	set_uniform(get_uniform_location(name), val);
+}
+
+void SdrProg::set_uniform(const char *name, float val)
+{
+	set_uniform(get_uniform_location(name), val);
+}
+
+void SdrProg::set_uniform(const char *name, const Vector2 &v)
+{
+	set_uniform(get_uniform_location(name), v);
+}
+
+void SdrProg::set_uniform(const char *name, const Vector3 &v)
+{
+	set_uniform(get_uniform_location(name), v);
+}
+
+void SdrProg::set_uniform(const char *name, const Vector4 &v)
+{
+	set_uniform(get_uniform_location(name), v);
+}
+
+void SdrProg::set_uniform(const char *name, const Matrix4x4 &mat)
+{
+	set_uniform(get_uniform_location(name), mat);
+}
+
+
+void SdrProg::set_uniform(int loc, int val)
+{
+	if(loc >= 0) {
+		bind_program(this);
+		glUniform1i(loc, val);
+	}
+}
+
+void SdrProg::set_uniform(int loc, float val)
+{
+	if(loc >= 0) {
+		bind_program(this);
+		glUniform1f(loc, val);
+	}
+}
+
+void SdrProg::set_uniform(int loc, const Vector2 &v)
+{
+	if(loc >= 0) {
+		bind_program(this);
+		glUniform2f(loc, v.x, v.y);
+	}
+}
+
+void SdrProg::set_uniform(int loc, const Vector3 &v)
+{
+	if(loc >= 0) {
+		bind_program(this);
+		glUniform3f(loc, v.x, v.y, v.z);
+	}
+}
+
+void SdrProg::set_uniform(int loc, const Vector4 &v)
+{
+	if(loc >= 0) {
+		bind_program(this);
+		glUniform4f(loc, v.x, v.y, v.z, v.w);
+	}
+}
+
+void SdrProg::set_uniform(int loc, const Matrix4x4 &mat)
+{
+	if(loc >= 0) {
+		bind_program(this);
+		// loading transpose matrix (3rd arg true)
+#ifdef SINGLE_PRECISION_MATH
+		glUniformMatrix4fv(loc, 1, GL_TRUE, (float*)mat.m);
+#else
+		glUniformMatrix4dv(loc, 1, GL_TRUE, (double*)mat.m);
+#endif
+	}
+}
+
+bool bind_program(const SdrProg *prog)
+{
+	if(!prog) {
+		glUseProgram(0);
+		return true;
+	}
+
+	if(!((SdrProg*)prog)->link()) {
+		return false;
+	}
+	glUseProgram(prog->prog);
+	return true;
+}
